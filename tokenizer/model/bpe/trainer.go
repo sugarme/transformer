2f12dda031e380dbb45f27e3f5fdbf432e8f786e
sugarme <thangtran@hotmail.com>
2020-03-09 20:12:03 +1100

    WIP: bpe/trainer

--- tokenizer/model/bpe/trainer.go
+++ tokenizer/model/bpe/trainer.go
@@ -1,1 +1,30 @@
 package bpe
+
+// Map with no value
+// Ref: https://stackoverflow.com/questions/57620170
+type UintSet map[uint]struct{}
+
+type CharSet map[rune]struct{}
+
+type TMerge struct {
+	Pair  Pair
+	Count uint32
+	Pos   UintSet
+}
+
+type TConfig struct {
+	MinFrequency            uint32
+	VocabSize               uint
+	ShowProgress            bool
+	SpecialTokens           []string
+	LimitAlphabet           *uint
+	InitialAlphabet         CharSet
+	ContinuingSubwordPrefix *string
+	EndOfWordSuffix         *string
+}
+
+// BpeTrainerBuilder can be used to create a `BpeTrainer`
+// with a custom configuration
+type BpeTrainerBuilder struct {
+	Config *Config
+}
