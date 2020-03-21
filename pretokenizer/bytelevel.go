233b364e9401fa67e945df03f8ae0c66e4eebfe2
sugarme <thangtran@hotmail.com>
2020-03-22 10:17:45 +1100

    WIP: ByteLevel pretokenize

--- pretokenizer/bytelevel.go
+++ pretokenizer/bytelevel.go
@@ -1,12 +1,31 @@
 package pretokenizer
 
 import (
+	"regexp"
+
+	"github.com/sugarme/sermo/normalizer"
+	"github.com/sugarme/sermo/tokenizer"
 	slice "github.com/sugarme/sermo/util/slice"
 )
 
+const constractRegStr = `'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+`
+
+var constractRE = regexp.MustCompile(constractRegStr)
+
+var BytesChar map[uint8]string = GenerateBytesChar()
+
+var CharBytes map[string]uint8 = func() map[string]uint8 {
+	var bc = GenerateBytesChar()
+	var cb map[string]uint8 = make(map[string]uint8)
+	for b, c := range bc {
+		cb[c] = b
+	}
+	return cb
+}()
+
 // BytesChar maps first 0-255 (byte) to first 0-255 `char` in unicode
 // Ref. https://en.wikipedia.org/wiki/List_of_Unicode_characters
-func BytesChar() map[uint8]string {
+func GenerateBytesChar() map[uint8]string {
 	var bs []uint8 // byte
 	var bc map[uint8]string = make(map[uint8]string)
 
@@ -35,4 +54,63 @@ func BytesChar() map[uint8]string {
 	}
 
 	return bc
+}
+
+// ByteLevel provides all the neccessary steps to handle the
+// BPE tokenization at byte-level. It takes care of all the required
+// processing steps to transform a utf-8 string as needed before and
+// after the BPE model does it job.
+type ByteLevel struct {
+	// whether to add a leading space to the first word.
+	// It allows to treat the leading word just as any other words.
+	AddPrefixSpace bool
+
+	// Whether the post processing step should trim offsets
+	// to avoid including whitespaces.
+	TrimOffsets bool
+}
+
+// NewByteLevel returns a default ByteLevel with both
+// AddPrefixSpace and TrimOffsets set true
+func NewByteLevel() *ByteLevel {
+	return &ByteLevel{
+		AddPrefixSpace: true,
+		TrimOffsets:    true,
+	}
+}
+
+// Alphabet returns set of first 256 unicode `char`
+func (bl *ByteLevel) Alphabet() map[string]struct{} {
+	var ab = make(map[string]struct{})
+	for _, c := range BytesChar {
+		ab[c] = struct{}{}
+	}
+
+	return ab
+}
+
+// SetAddPrefixSpace set `AddPrefixSpace` property
+func (bl *ByteLevel) SetAddPrefixSpace(v bool) {
+	bl.AddPrefixSpace = v
+}
+
+// SetTrimOffsets set `TrimOffsets` property
+func (bl *ByteLevel) SetTrimOffsets(v bool) {
+	bl.TrimOffsets = v
+}
+
+// Implement `PreTokenizer` methods for `ByteLevel`
+
+type PreTokResult struct {
+	Content string
+	Offsets tokenizer.Offsets
+}
+
+// PreTokenize is in charge of transforming all the unicode
+// characters into their byte-level counterpart. It also splits
+// the input according to the configured regex.
+func (bl *ByteLevel) PreTokenize(normalized normalizer.NormalizedString) *PreTokResult {
+	// TODO: implement detail
+
+	return &PreTokResult{}
 }
--- pretokenizer/bytelevel_test.go
+++ pretokenizer/bytelevel_test.go
@@ -11,12 +11,29 @@ func TestBytesChar(t *testing.T) {
 
 	want := "!"
 
-	bc := pretokenizer.BytesChar()
+	bc := pretokenizer.GenerateBytesChar()
 	got := bc[33]
 
 	if !reflect.DeepEqual(want, got) {
 		t.Errorf("Want: %v\n", want)
 		t.Errorf("Got: %v\n", got)
+	}
+
+	// Test generating bytesChar map
+	bcGot := pretokenizer.BytesChar[33]
+	// bcWant := make(map[uint8]string)
+	bcWant := "!"
+	if !reflect.DeepEqual(bcWant, bcGot) {
+		t.Errorf("Want: %v\n", bcWant)
+		t.Errorf("Got: %v\n", bcGot)
+	}
+
+	// Testing generate charBytes map
+	cbGot := pretokenizer.CharBytes["!"]
+	var cbWant uint8 = 33
+	if !reflect.DeepEqual(cbWant, cbGot) {
+		t.Errorf("Want: %v\n", cbWant)
+		t.Errorf("Got: %v\n", cbGot)
 	}
 
 }
