abfbdddc39a3e04698bb5d8ef97dd21df9de13a7
sugarme <thangtran@hotmail.com>
2020-10-02 10:32:48 +1000

    feat(roberta/embeddings): completed

---
+++ util/tensor.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"reflect"
+
+	ts "github.com/sugarme/gotch/tensor"
+)
+
+// Equal compares 2 tensors in terms of shape, and every element values.
+func Equal(tensorA, tensorB ts.Tensor) bool {
+	var equal int64 = 0
+	// 1. Compare shape
+	if reflect.DeepEqual(tensorA.MustSize(), tensorB.MustSize()) {
+		// 2. Compare values
+		equal = tensorA.MustEq1(tensorB, false).MustAll(false).Int64Values()[0]
+	}
+	if equal == 0 {
+		return false
+	}
+	return true
+}
