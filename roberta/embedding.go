6cba3bf76217881e31af174d30aaa43e09b62cee
sugarme <thangtran@hotmail.com>
2020-10-02 10:32:48 +1000

    feat(roberta/embeddings): completed

--- roberta/embedding.go
+++ roberta/embedding.go
@@ -1,6 +1,8 @@
 package roberta
 
 import (
+	"fmt"
+
 	"github.com/sugarme/gotch"
 	"github.com/sugarme/gotch/nn"
 	ts "github.com/sugarme/gotch/tensor"
@@ -72,20 +74,74 @@ func NewRobertaEmbeddings(p nn.Path, config *bert.BertConfig) *RobertaEmbeddings
 // This differs from the original BERT embeddings in how the position ids are calculated when not provided.
 //
 // Params:
-// 	- `inputIds` - Optional input tensor of shape (batch size, sequence length).
+// 	- `inputIds`: Optional input tensor of shape (batch size, sequence length).
 // 		If None, pre-computed embeddings must be provided (see `inputEmbeds`)
-// 	- `tokenTypeIds` -Optional segment id of shape (batch size, sequence length).
+// 	- `tokenTypeIds`: Optional segment id of shape (batch size, sequence length).
 // 		Convention is value of 0 for the first sentence (incl. [SEP]) and 1 for the second sentence. If None set to 0.
-// 	- `positionIds` - Optional position ids of shape (batch size, sequence length).
+// 	- `positionIds`: Optional position ids of shape (batch size, sequence length).
 // 		If None, will be incremented from 0.
-// 	- `inputEmbeds` - Optional pre-computed input embeddings of shape (batch size, sequence length, hidden size).
+// 	- `inputEmbeds`: Optional pre-computed input embeddings of shape (batch size, sequence length, hidden size).
 // 		If None, input ids must be provided (see `inputIds`)
-// 	- `train` - boolean flag to turn on/off the dropout layers in the model.
+// 	- `train`: boolean flag to turn on/off the dropout layers in the model.
 //		Should be set to false for inference.
 //
-// Returns:
-// 	- `embeddedOutput` - `Tensor` of shape (*batch size*, *sequence_length*, *hidden_size*)
-func (re *RobertaEmbeddings) ForwardT(inputIds, tokenTypeIds, positionIds, inputEmbeds ts.Tensor, train bool) (ts.Tensor, string) {
+// Return:
+// 	- `embeddedOutput`: tensor of shape (batch size, sequence length, hidden size)
+func (re *RobertaEmbeddings) ForwardT(inputIds, tokenTypeIds, positionIds, inputEmbeds ts.Tensor, train bool) (ts.Tensor, error) {
 
-	panic("not implemented yet")
+	var (
+		inputEmbeddings ts.Tensor
+		inputShape      []int64
+	)
+
+	if !inputIds.MustDefined() {
+		if inputEmbeds.MustDefined() {
+			inputEmbeddings = inputEmbeds
+			inputEmbedsShape := inputEmbeds.MustSize()
+			inputShape = []int64{inputEmbedsShape[0], inputEmbedsShape[1]}
+		} else {
+			err := fmt.Errorf("Only one of input Ids or input embeddings may be set.")
+			return ts.None, err
+		}
+	} else {
+		// if inputIds == inputEmbeds
+		if util.Equal(inputIds, inputEmbeds) {
+			err := fmt.Errorf("Only one of input Ids or input embeddings may be set.")
+			return ts.None, err
+		} else {
+			inputEmbeddings = inputIds.ApplyT(re.wordEmbeddings, train)
+			inputShape = inputIds.MustSize()
+		}
+	}
+
+	var posIds ts.Tensor
+	if positionIds.MustDefined() {
+		posIds = positionIds
+	} else {
+		if inputIds.MustDefined() {
+			posIds = re.createPositionIdsFromInputIds(inputIds)
+		} else {
+			posIds = re.createPositionIdsFromEmbeddings(inputEmbeds)
+		}
+	}
+
+	var tokTypeIds ts.Tensor
+	if tokenTypeIds.MustDefined() {
+		tokTypeIds = tokenTypeIds
+	} else {
+		tokTypeIds = ts.MustZeros(inputShape, gotch.Int64, inputEmbeddings.MustDevice())
+	}
+
+	positionEmbeddings := posIds.Apply(re.positionEmbeddings)
+	tokenTypeEmbeddings := tokTypeIds.Apply(re.tokenTypeEmbeddings)
+
+	add1 := inputEmbeddings.MustAdd(positionEmbeddings, true)
+	newInputEmbeddings := add1.MustAdd(tokenTypeEmbeddings, true)
+	inputEmbeddings.MustDrop()
+
+	appliedLN := newInputEmbeddings.Apply(re.layerNorm)
+	appliedDropout := appliedLN.ApplyT(re.dropout, train)
+	appliedLN.MustDrop()
+
+	return appliedDropout, nil
 }
